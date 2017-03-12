e68a824ea6b73f204bef9373ff124bd19f185828
Matthias Hüller <matthias.hueller@1und1.de>
2017-03-12 16:40:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+camunda-springboot
+==================
+
+Arbeitsprojekt für die Vorlesung eBusiness Anwendungsentwicklung (HS Ansbach, Sommersemester 2017)
+
+1) Anforderungen Entwicklungsumgebung
+   - Java SE 8 (http://www.oracle.com/technetwork/java/javase/downloads/index.html)
+   - Maven 3 (http://maven.apache.org/)
+   - Spring Toolsuite (https://spring.io/tools)
+   - Camunda Modeler (https://camunda.org/download/modeler/)
+   
+2) Kontrolle Entwicklungsumgebung per Kommandozeile
+   - java -version
+   - mvn -version
---
+++ src/main/java/de/hsansbach/ecommerce/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,45 @@
+package de.hsansbach.ecommerce.configuration;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// @formatter:off
+		http
+			.csrf()
+				.disable() // CSRF protection is not supported by Camunda Webapps
+			.formLogin()
+				.loginPage("/login")
+				.failureUrl("/login?error")
+				.and()
+			.authorizeRequests()
+				.antMatchers("/login").permitAll()
+				.antMatchers("/app/**", "/lib/**", "/api/**").permitAll() // Required by Camunda Webapps
+				.antMatchers("/messages/**").hasAnyRole("ADMIN", "USER")
+				.anyRequest().authenticated();
+		// @formatter:on
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// @formatter:off
+		auth
+			.inMemoryAuthentication()
+				.withUser("admin").password("admin").roles("ADMIN", "USER")
+				.and()
+				.withUser("kermit").password("kermit").roles("USER")
+				.and()
+				.withUser("gonzo").password("gonzo").roles("USER");
+		// @formatter:on
+	}
+
+}
