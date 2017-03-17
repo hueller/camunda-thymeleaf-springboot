9cae4ef576f09d7236732793f438a4ed575f0fe5
Matthias Hüller <matthias.hueller@1und1.de>
2017-03-17 21:52:07 +0100

    Fix process test.

--- src/test/java/de/hsansbach/ecommerce/process/SampleProcessTest.java
+++ src/test/java/de/hsansbach/ecommerce/process/SampleProcessTest.java
@@ -5,8 +5,11 @@ import static org.junit.Assert.assertEquals;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.impl.ProcessEngineImpl;
+import org.camunda.bpm.engine.impl.test.TestHelper;
 import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.junit.Test;
@@ -20,6 +23,9 @@ import de.hsansbach.ecommerce.CamundaSpringbootApplication;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = CamundaSpringbootApplication.class)
 public class SampleProcessTest extends AbstractProcessTest {
+	
+	@Autowired
+	private ProcessEngine processEngine;
 
 	@Autowired
 	private RuntimeService runtimeService;
@@ -31,6 +37,8 @@ public class SampleProcessTest extends AbstractProcessTest {
 	@Deployment
 	public void simpleProcessTest() {
 		runtimeService.startProcessInstanceByKey("Sample", defaultVariables());
+		
+		TestHelper.waitForJobExecutorToProcessAllJobs(((ProcessEngineImpl) processEngine).getProcessEngineConfiguration(), 10000, 50);
 
 		Task task = taskService.createTaskQuery().singleResult();
 		assertEquals("Confirmation User Task", task.getName());
