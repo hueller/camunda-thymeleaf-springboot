459aad464797f26aad2bd10173147ca9eef6e50d
Matthias Hüller <matthias.hueller@1und1.de>
2017-03-18 17:11:18 +0100

    Added bean validations using Hibernate validator.

--- src/main/java/de/hsansbach/ecommerce/mvc/model/ProcessModel.java
+++ src/main/java/de/hsansbach/ecommerce/mvc/model/ProcessModel.java
@@ -1,10 +1,13 @@
 package de.hsansbach.ecommerce.mvc.model;
 
+import javax.validation.constraints.Size;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class ProcessModel {
 
 	@NotEmpty(message = "Text is required.")
+	@Size(min=1, max=20)
 	private String text;
 
 	public String getText() {
